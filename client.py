110ad45339dffe6cc6d71db6085c19299c1c72da
chenhao1106 <49955647+chenhao1106@users.noreply.github.com>
2019-12-11 20:06:24 +0800

    network

---
+++ client.py
@@ -0,0 +1,116 @@
+import pyaudio
+import socket
+import sys
+from threading import Thread
+import time
+import numpy
+import cv2
+
+#音訊格式
+FORMAT = pyaudio.paInt16 #取樣格式
+CHANNELS = 1 #聲道
+RATE = 44100 #取樣率(Hz)
+CHUNK = 1024 #緩衝大小(frame)
+
+HOST = "192.168.43.203"
+PORT1 = 3456
+PORT2 = 3457
+VIDEO_PORT = 3458
+def sending() :	
+	clientSocket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	clientSocket1.connect((HOST, PORT1))
+
+	def callback(in_data, frame_count, time_info, status):
+		clientSocket1.send(in_data)
+		return (None, pyaudio.paContinue)
+
+
+	audio = pyaudio.PyAudio()
+	stream = audio.open(format = FORMAT, channels = CHANNELS, rate = RATE, input = True , frames_per_buffer = CHUNK)
+	try:
+		while True:
+			sending_data = stream.read( CHUNK )
+			clientSocket1.sendall(sending_data)
+			if cv2.waitKey(1) & 0xFF == ord('q'):  #按q键退出
+				break
+	except KeyboardInterrupt:
+		pass	    
+
+		
+	clientSocket1.close()
+	stream.stop_stream()
+	stream.close()
+	audio.terminate()
+
+def receiving() :
+	clientSocket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	clientSocket2.connect((HOST, PORT2))
+	print("Connected 2")
+	audio2 = pyaudio.PyAudio()
+	stream2 = audio2.open(format = FORMAT, channels = CHANNELS, rate = RATE, output = True, frames_per_buffer = CHUNK)
+	print("Start")
+	try:
+		while True:
+			data = clientSocket2.recv(CHUNK)
+			stream2.write( data )
+			if cv2.waitKey(1) & 0xFF == ord('q'):  #按q键退出
+				break
+	except KeyboardInterrupt:
+		pass       
+
+					
+	clientSocket2.close()
+	stream2.close()
+	audio2.terminate()
+
+audio_thread_in = Thread( target = receiving )
+audio_thread_in.setDaemon(True)
+audio_thread_in.start()
+time.sleep(0.1)
+
+audio_thread_out = Thread( target = sending )
+audio_thread_out.setDaemon(True)
+audio_thread_out.start()
+
+time.sleep(0.1)
+
+camera = cv2.VideoCapture(0)
+ret, frame = camera.read()
+
+videoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+videoSocket.connect((HOST, VIDEO_PORT))
+encode_param=[int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+def recvall(sock, count):
+	buf = b''
+	while count:
+		newbuf = sock.recv(count)
+		if not newbuf: return None
+		buf += newbuf
+		count -= len(newbuf)
+	return buf
+
+while ret:
+	result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+	sending_data = numpy.array(imgencode)
+	sending_stringData = sending_data.tostring()
+	byte = str(len(sending_stringData)).ljust( 16 )
+	videoSocket.send(byte.encode());
+	videoSocket.send(sending_stringData);
+	ret, frame = camera.read()
+	
+	#  收server資料
+	length = recvall(videoSocket, 16)
+	received_stringData = recvall(videoSocket, int(length))
+	received_data = numpy.fromstring(received_stringData, dtype='uint8')
+	received_decimg = cv2.imdecode(received_data, 1)
+	cv2.imshow('Server', received_decimg)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):  #按q键退出
+		break
+	
+	
+
+videoSocket.close()
+cv2.destroyAllWindows()
+camera.release()
