efe8f53ff25a18964575abfcc2e8058be8513aa3
chenhao1106 <49955647+chenhao1106@users.noreply.github.com>
2019-12-25 23:34:58 +0800

    Financial report crawler

--- Financial report crawler.py
+++ Financial report crawler.py
@@ -3,8 +3,17 @@ import pandas as pd
 
 
 def crwal_financial_report(year, season, sid):
+	"""
+	此函式抓取股票財報
+	year(int):抓取年份
+	season(int):抓取季節
+	sid(int):股票編號
+	"""
+
+	# 財報網站
 	FinanceReportURL = "https://mops.twse.com.tw/mops/web/t163sb01"
 
+	# request資訊
 	form_data = {
 		'encodeURIComponent':1,
         'step':1,
@@ -23,4 +32,5 @@ def crwal_financial_report(year, season, sid):
 
 report = crwal_financial_report(108, 3, 2330).fillna("")
 # report.to_excel("Financial Report.xlsx", header=False, sheet_name=str(stock_number), index=False, startrow = 0, encoding="utf_8_sig")
+
 print(report)
