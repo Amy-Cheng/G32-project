531a7da9ba7a653596d696ff2d46c314fff31c7f
fred12345tw <fred12345tw@gmail.com>
2019-12-26 21:28:21 +0800

    Update 爬蟲.py
    加上可以查5日20日均線的程式碼

--- 爬蟲.py
+++ 爬蟲.py
@@ -1,6 +1,7 @@
 #需要先安裝lxml,requests,twstock 三個套件
 # pip install stock
 import twstock
+twstock.__update_codes()
 oneormore = input("若要查詢單股請按y,多股按任意鍵:")
 one = oneormore
 #輸入要一次搜尋一檔還是多檔
@@ -12,7 +13,9 @@ if one == "y":
     # 輸入查詢天數
     days = int(daysstr)
     stock = twstock.Stock(stockenter)
-    print(stock.price[-days:])
+    mean5 = stock.moving_average(stock.price, 5)
+    mean20 = stock.moving_average(stock.price, 20)
+    print("近",days."日股價:" , stock.price[-days:] , "近",days,"日的五日均線" ,  mean5[-days:], "近" , days , "日的二十日均線" , mean20[-days:])
     # 印出股價
 else:
     stockenter = input("請輸入股票代號,代號間用逗號隔開:")
@@ -24,8 +27,9 @@ else:
     #print(stockdata[stocklist[0]]["realtime"].keys())
     for i in range(len(stocklist)):    
         
-        print(stocklist[i]+":"+stockdata[stocklist[i]]["realtime"]["latest_trade_price"]
-        ,stockdata[stocklist[i]]["info"]["name"])
-        
+        print(stockdata[stocklist[i]]["info"]["name"]+"("+stocklist[i]+")"+":"+" 收盤價:"+stockdata[stocklist[i]]["realtime"]["latest_trade_price"]
+        +" 開盤價:"+stockdata[stocklist[i]]["realtime"]["open"]+" 最高價:"+stockdata[stocklist[i]]["realtime"]["high"]
+        +" 最低價:"+stockdata[stocklist[i]]["realtime"]["low"])
+        print(stockdata[stocklist[i]])
 
 
