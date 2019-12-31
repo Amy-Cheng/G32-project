f0fe852d6741461e1bcd1245440d133d7fc279b5
jimchen4 <58811434+jimchen4@users.noreply.github.com>
2019-12-31 20:54:24 +0800

    Add files via upload
    revised by jim(改x軸時間呈現格式)

---
+++ 籌碼資料 - 複製.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+import requests
+import pandas as pd
+import json
+import numpy as np
+from datetime import datetime, timedelta
+import scrapy
+from bokeh.plotting import figure, output_file, show
+from bokeh.io import show
+from bokeh.models import ColumnDataSource
+from bokeh.models import HoverTool
+from bokeh.models import DatetimeTickFormatter
+
+date1 = input("日期：")
+sid = input("股票代碼：")
+# items作爲天數的計數器
+items = 0
+# y_axis作爲存放股數的list
+y_axis = []
+
+def workday(date):  # 判斷是否為工作日
+    global json_data
+    dtnb = {'response': 'json', 'date': date, 'selectType': 'ALLBUT0999', '_' : '1577233837182'}
+    json_data=requests.get("https://www.twse.com.tw/fund/T86", dtnb).json()
+    if json_data == {'stat': '很抱歉，沒有符合條件的資料!'}:
+        return False
+    else:
+        return json_data
+
+
+def show_data(json_data):
+    global data
+    columns= ["證券代號","證券名稱","外陸資買進股數(不含外資自營商)","外陸資賣出股數(不含外資自營商)",
+    "外陸資買賣超股數(不含外資自營商)","外資自營商買進股數","外資自營商賣出股數","外資自營商買賣超股數",
+    "投信買進股數","投信賣出股數","投信買賣超股數","自營商買賣超股數","自營商買進股數(自行買賣)",
+    "自營商賣出股數(自行買賣)","自營商買賣超股數(自行買賣)","自營商買進股數(避險)",
+    "自營商賣出股數(避險)","自營商買賣超股數(避險)","三大法人買賣超股數"]
+
+    data = pd.DataFrame(json_data['data'],columns=columns)
+    data = data[["證券代號","外陸資買賣超股數(不含外資自營商)","外資自營商買賣超股數",
+    "投信買賣超股數","自營商買賣超股數"]]
+    data.iloc[:, 1:] = data.iloc[:, 1:].applymap(lambda x: x.replace(",", ""))
+    data.iloc[:, 1:] = data.iloc[:, 1:].applymap(lambda x: int(x))
+    data.iloc[:, 1:] = data.iloc[:, 1:]/1000
+    data = data.rename(columns={"外陸資買賣超股數(不含外資自營商)":"外陸資買賣超股數"})
+
+if workday(date1) != False:
+    show_data(json_data)
+    for id in range(data.shape[0]):
+        if data.iloc[id, 0] == sid:
+			# 存放第一天的四個資料
+            y_axis.append(data.iloc[id, 1])
+            y_axis.append(data.iloc[id, 2])
+            y_axis.append(data.iloc[id, 3])
+            y_axis.append(data.iloc[id, 4])
+            items = 1
+            break
+# 新增4個list作爲4種資料各自存放的list
+foreign_investors = []
+foreign_dealers = []
+investment_trust = []
+local_dealers= []
+d1 = datetime(int(date1[0:4]), int(date1[4:6]), int(date1[6:]))
+d2 = d1 + timedelta(days = -1)
+date_before = d2.strftime("%Y%m%d")
+x_axis = [d1]
+# 重複執行直到7天的數據已經存放好
+while items != 7:
+    if workday(date_before) != False:
+        print(date_before)
+        show_data(json_data)
+        for id in range(data.shape[0]):
+            if data.iloc[id, 0] == sid:
+				# 根據對應天數，存放四個資料
+                y_axis.append(data.iloc[id, 1])
+                y_axis.append(data.iloc[id, 2])
+                y_axis.append(data.iloc[id, 3])
+                y_axis.append(data.iloc[id, 4])
+                print(y_axis)
+                items += 1
+                x_axis.append(d2)
+                d2 = d2 + timedelta(days = -1)
+                date_before = d2.strftime("%Y%m%d")
+    else:
+        d2 = d2 + timedelta(days = -1)
+        date_before = d2.strftime("%Y%m%d")
+
+# 將第一天掉換成最後一天，因x軸需要符合時間綫概念
+x_axis.reverse()
+y_axis.reverse()
+print(x_axis)
+for i in range(0,25,4):
+	# 因數據已經掉換，所以用反序來擷取資料
+    foreign_investors.append(y_axis[i+3])
+    foreign_dealers.append(y_axis[i+2])
+    investment_trust.append(y_axis[i+1])
+    local_dealers.append(y_axis[i])
+p = figure(title="各方買賣超股數", x_axis_type = 'datetime')
+# 畫出4條綫代表各自的資料
+p.line(x_axis, foreign_investors, legend="外陸資買賣超股數", line_color="red")
+p.line(x_axis, foreign_dealers, legend="外資自營商買賣超股數", line_color="blue")
+p.line(x_axis, investment_trust, legend="投信買賣超股數", line_color="green")
+p.line(x_axis, local_dealers, legend="自營商買賣超股數", line_color="#9B870C")
+# 標上軸名
+p.legend.location = "top_left"
+p.xaxis.axis_label = '日期'
+p.yaxis.axis_label = '股數'
+p.xaxis.formatter = DatetimeTickFormatter(days=["%Y%m%d"])
+show(p)
